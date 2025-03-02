6242b0c6ad59afb93c311101c16a692d813c9035
Jacobi RTX Server <it@jacobi.ai>
2025-03-03 01:25:35 +0800

    Update batch generate script

--- scripts/batch_generate.sh
+++ scripts/batch_generate.sh
@@ -11,6 +11,7 @@ OLLAMA_HOST="http://localhost:11434"
 
 function ensure_success() {
     if [[ $? -ne 0 ]]; then
+        echo ""
         echo "ERROR: Exception detected. (DP_SCENES: $DP_SCENES, DP_OBJECTS: $DP_OBJECTS, DATASET_TYPE: $DATASET_TYPE)"
         exit 1;
     fi
